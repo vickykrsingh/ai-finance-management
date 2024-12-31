e1e4a77b3db81816d85958cd1a7163d414bb89e2
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-31 11:07:20 +0530

    hero section

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,32 +1,55 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
 function HeroSection() {
+    const imageRef = useRef();
+
+    useEffect(()=>{
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+
+            if(scrollPosition>scrollThreshold){
+                imageElement.classList.add("scrolled")
+            }else{
+                imageElement.classList.remove("scrolled")
+            }
+        }
+
+        window.addEventListener("scroll",handleScroll)
+
+        return () => window.removeEventListener("scroll",handleScroll)
+
+    },[]);
   return (
     <div className='pb-20 px-4'>
-        <div>
-            <h1>
+        <div className='container mx-auto text-center'>
+            <h1 className='text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title'>
                 Manage Your Finances <br/> with Intelligence
             </h1>
-            <p>
+            <p className='text-xl text-gray-600 max-w-2xl mx-auto'>
                 An AI-powered financial management platform that helps you track, analyze, and optimize your spending with real-time insights
             </p>
-            <div>
-                <Link href={'/dashboard'}>
-                    <Button size="lg" className="px-8">
-                        Get Started
-                    </Button>
-                </Link>
-                <Link href={'/youtube.com'}>
-                    <Button size="lg" className="px-8">
-                        Watch Demo
-                    </Button>
-                </Link>
-                <div>
-                    <div>
+            <div className='flex flex-col justify-center space-x-4 my-6'>
+                <div className='flex gap-2 justify-center'>
+                    <Link href={'/dashboard'}>
+                        <Button size="lg" className="px-8">
+                            Get Started
+                        </Button>
+                    </Link>
+                    <Link href={'/youtube.com'}>
+                        <Button size="lg" className="px-8">
+                            Watch Demo
+                        </Button>
+                    </Link>
+                </div>
+                <div className='hero-image-wrapper'>
+                    <div ref={imageRef} className='hero-image'>
                         <Image src="/banner.jpeg" width={1280} height={720} alt='Dashboard Preview' className='rounded-lg shadow-2xl border mx-auto' priority />
                     </div>
                 </div>

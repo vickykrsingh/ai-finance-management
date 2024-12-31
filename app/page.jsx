e11054252713300dc9744cabeff45b807ae24899
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-31 12:06:41 +0530

    added features section inside home section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
-import { statsData } from "@/data/landing";
+import { Card, CardContent } from "@/components/ui/card";
+import { featuresData, statsData } from "@/data/landing";
 
 export default function Home() {
   return (
@@ -11,8 +12,8 @@ export default function Home() {
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
             {
-              statsData.map((stats)=>(
-                <div key={stats.label} className="text-center">
+              statsData.map((stats,index)=>(
+                <div key={index} className="text-center">
                   <div className="text-4xl font-bold text-blue-600 mb-2">{stats.label}</div>
                   <div className="text-gray-600">{stats.value}</div>
                 </div>
@@ -21,7 +22,23 @@ export default function Home() {
           </div>
         </div>
       </section>
-      
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2  className="text-3xl font-bold text-center mb-12">Everythings you need to manage your finances</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {featuresData.map((features,index)=>(
+            <Card key={index} className="p-6">
+              <CardContent className="space-y-4 pt-4">
+                {features.icon}
+                <h3 className="text-xl font-semibold">{features.title}</h3>
+                <p className="text-gray-600">{features.description}</p>
+              </CardContent>
+            </Card>            
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

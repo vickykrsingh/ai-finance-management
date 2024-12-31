e25b80904249a99605862fd7a2ebae65391d3e96
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-31 12:14:33 +0530

    How it works section inside home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { featuresData, statsData } from "@/data/landing";
+import { featuresData, howItWorksData, statsData } from "@/data/landing";
 
 export default function Home() {
   return (
     <div className="mt-40">
+      {/* Hero section */}
       <HeroSection/>
-
+      {/* Stats section */}
       <section className="py-20 bg-blue-50">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
@@ -22,7 +23,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
+      {/* Features section */}
       <section className="py-20">
         <div className="container mx-auto px-4">
           <h2  className="text-3xl font-bold text-center mb-12">Everythings you need to manage your finances</h2>
@@ -35,6 +36,21 @@ export default function Home() {
                 <p className="text-gray-600">{features.description}</p>
               </CardContent>
             </Card>            
+            ))}
+          </div>
+        </div>
+      </section>
+      {/* How it works section */}
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <h2  className="text-3xl font-bold text-center mb-12">How It Works</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {howItWorksData.map((step,index)=>(
+              <div key={index} className="text-center">
+                <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-6">{step.icon}</div>
+                <h3 className="text-xl font-semibold mb-4">{step.title}</h3>
+                <p className="text-gray-600">{step.description}</p>
+              </div>
             ))}
           </div>
         </div>

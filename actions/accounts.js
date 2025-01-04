606e3435b7eda3d8059be155fa18315208acc8ef
vickykrsingh27 <vickykrsingh27@gmail.com>
2025-01-04 08:01:07 +0530

    seeding transaction data for account page design

--- actions/accounts.js
+++ actions/accounts.js
@@ -33,21 +33,56 @@ export async function updateDefaultAccount(accountId) {
     }
 
     await db.account.updateMany({
-        where:{userId:user.id,isDefault:true},
-        data:{isDefault:false}
-    })
+      where: { userId: user.id, isDefault: true },
+      data: { isDefault: false },
+    });
 
     const account = await db.account.update({
-        where:{
-            id:accountId,
-            userId:user.id
-        },
-        data:{isDefault:true}
-    })
-    revalidatePath('/dashboard');
-    const serilazeData = account.map((acc)=>serializeTransaction(acc))
-    return {success:true,data:serilazeData}
+      where: {
+        id: accountId,
+        userId: user.id,
+      },
+      data: { isDefault: true },
+    });
+    revalidatePath("/dashboard");
+    return { success: true, data: serializeTransaction(account) };
   } catch (error) {
-    return {success:false,error:error.message}
+    return { success: false, error: error.message };
+  }
+}
+
+export async function getAccountWithTransactions(accountId) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const account = await db.account.findUnique({
+      where:{id:accountId,userId:user.id},
+      include:{
+        transactions:{
+          orderBy:{date:"desc"}
+        },
+        _count:{
+          select:{transactions:true}
+        },
+      }
+    })
+
+    if(!account) return null;
+
+    return {
+      ...serializeTransaction(account),
+      transactions:account.transactions.map(serializeTransaction)
+    }
+  } catch (error) {
+    return null;
   }
 }
--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { getAccountWithTransactions } from "@/actions/accounts";
+import notfound from "@/app/not-found";
+import React from "react";
 
-function UserAccount({id}) {
-return (
-    <div>{id} hello</div>
-  )
+async function UserAccount({ params }) {
+  const { id } = await params;
+  const accountData = await getAccountWithTransactions(id);
+
+  if (!accountData) {
+    return notfound();
+  }
+
+  const { transaction, ...account } = accountData;
+  return (
+    <div className="space-y-8 px-5 flex gap-4 items-end justify-between">
+      <div>
+        <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">{account.name}</h1>
+        <p className="text-muted-foreground">
+          {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
+        </p>
+      </div>
+      <div className="pb-2 text-right">
+        <div className="text-xl sm:text-2xl font-bold">${parseFloat(account.balance).toFixed(2)}</div>
+        <p className="text-sm text-muted-foreground">{account._count.transactions} Transaction</p>
+      </div>
+      {/* Chart section */}
+
+
+      {/* Transaction section */}
+    </div>
+  );
 }
 
-export default UserAccount
+export default UserAccount;

9292de6a0ba8923c981bfa7886716cc67b91ae78
vickykrsingh27 <vickykrsingh27@gmail.com>
2025-01-13 17:42:59 +0530

    Recurring Transaction CRON using Inngest Function

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,17 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert } from "@/lib/inngest/functions";
+import {
+  checkBudgetAlert,
+  processRecurringTransaction,
+  triggerRecurringtransactions,
+} from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlert
+    checkBudgetAlert,
+    triggerRecurringtransactions,
+    processRecurringTransaction,
   ],
 });

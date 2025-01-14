0b903ecd7662593f94fc5e07660cf94cd3917acf
vickykrsingh27 <vickykrsingh27@gmail.com>
2025-01-14 14:28:37 +0530

    middleware error resolved

--- middleware.js
+++ middleware.js
@@ -1,4 +1,3 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -7,30 +6,13 @@ const isProtectedRoute = createRouteMatcher([
     "/transaction(.*)"
 ]);
 
-const aj = arcjet({
-  key:process.env.ARCJET_KEY,
-  rules:[
-    shield({
-      mode:'LIVE'
-    }),
-    detectBot({
-      mode:'LIVE',
-      allow:[
-        "CATEGORY:SEARCH_ENGINE","GO_HTTP"
-      ]
-    })
-  ]
-})
-
-const clerk = clerkMiddleware(async (auth,req)=>{
+export default clerkMiddleware(async (auth,req)=>{
     const {userId} = await auth();
     if(!userId && isProtectedRoute(req)){
         const {redirectToSignIn} = await auth();
         return redirectToSignIn();
     }
 });
-
-export default createMiddleware(aj,clerk)
 
 export const config = {
   matcher: [
